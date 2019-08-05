468b94cf42cb8241f8dd2c180e06a94150121f0b
AlexGoAhead <alex.8979323846@gmail.com>
2019-08-05 18:23:10 +0300

    exercise 3

---
+++ Program3.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace ConsoleApp3
+{
+    class Program3
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter any number: ");
+            int selection = Convert.ToInt32(Console.ReadLine());
+
+            if (selection == 5 || selection == 10)
+            {
+                Console.WriteLine("The number is equal 5 or 10");
+            }
+            else
+            {
+                Console.WriteLine("Unknown number");
+            }
+            Console.ReadKey();
+        }
+    }
+}
+
+//class Program3
+//{
+//    static void Main(string[] args)
+//    {
+//        Console.WriteLine("Enter any number: ");
+//        int selection = Convert.ToInt32(Console.ReadLine());
+//        switch (selection)
+//        {
+//            case (5):
+//                Console.WriteLine("The number equal 5 or 10!");
+//                break;
+//            case (10):
+//                Console.WriteLine("The number equal 5 or 10!");
+//                break;
+//            default:
+//                Console.WriteLine("Unknown number.");
+//                break;
+//        }
+//        Console.ReadKey();
+//    }
+//}
