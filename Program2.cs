44d27afffceabb4523ff28e5a7b826009a35815a
AlexGoAhead <alex.8979323846@gmail.com>
2019-08-05 17:48:18 +0300

    exercise 2

---
+++ Program2.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class Program2
+    {
+        static void Main1(string[] args)
+        {
+            Console.WriteLine("Enter a number in range 1 - 10");
+            int selection = Convert.ToInt32(Console.ReadLine());
+            if (selection > 5 && selection < 10)
+            { Console.WriteLine($"The number {selection} bigger than 5 and smaller than 10!"); }
+            else
+            { Console.WriteLine("Unknown number."); }
+            Console.ReadKey();
+        }
+    }
+}
